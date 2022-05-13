af7179213bd68fd7a10f9af227a853ae6649b6d0
Bricio De jesus Sanchez <bricio3599@gmail.com>
2022-05-13 14:09:23 -0500

    CREACION DE PROYECTO

---
+++ TipoTriangulos/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TipoTriangulos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Averiguar el tipo de triangulo 
+            int LADO1, LADO2, LADO3;
+            string linea;
+            Console.WriteLine("Digite el lado 1: "); linea = Console.ReadLine();
+            LADO1 = int.Parse(linea);
+
+            Console.WriteLine("Digite el lado 2:"); linea = Console.ReadLine();
+            LADO2 = int.Parse(linea);   
+
+            Console.WriteLine("Digite el lado 3"); linea = Console.ReadLine();
+            LADO3 = int .Parse(linea);  
+
+            if ((LADO1 == LADO2 ) & (LADO2 == LADO3))
+            {
+                Console.WriteLine("TRIANGULO EQUILATERO. TODOS IGUALES");
+            }
+
+            else
+            {
+                if ((LADO1 != LADO2) & (LADO1 != LADO3) & (LADO2 != LADO3 ))
+                {
+                    Console.WriteLine("TRIANGULO ESCALENO. NINGUNO IGUAL");
+                }
+                else
+                {
+                    Console.WriteLine("TRIANGULO ISOSCELES. DOS LADOS IGUALES");
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
